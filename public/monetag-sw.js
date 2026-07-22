e15cea81ba977347d8ae89ffc1d3b7cf70badce9
primemar-final <supremealpha04@gmail.com>
2026-07-22 07:26:40 +0100

    Create monetag-sw.js

---
+++ public/monetag-sw.js
@@ -0,0 +1,9 @@
+// Monetag Service Worker
+// This file must be in your public directory
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11365022
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
