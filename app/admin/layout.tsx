61006e36dc15ba345f19f9c9e2c28993b8481546
primemar-final <supremealpha04@gmail.com>
2026-08-01 16:12:51 +0100

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import './layout.css'
 import { 
   LayoutDashboard, 
   Users, 
