2906d0afd13a4c38d9caa4fee169c56290c90878
primemar-final <supremealpha04@gmail.com>
2026-08-31 15:16:36 -0700

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,9 +2,9 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export const createServerSupabaseClient = () => {
+export function createClient() {
   const cookieStore = cookies()
-
+  
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
